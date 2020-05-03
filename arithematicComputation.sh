c4cbf0d3656634ae21038a7feab67dd23669c10e
vinitathopte2603 <vinitathopte1@outlook.com>
2020-05-03 20:17:44 +0530

    [Vinita]ADDED:DictionaryToStoreResultOfEachOperation

--- arithematicComputation.sh
+++ arithematicComputation.sh
@@ -8,15 +8,22 @@ read -p "Enter b value : " b
 read -p "Enter c value : " c
 
 echo "$a $b $c"
-
+declare -A result
+counter=0
 firstOperation=$(( $a+$(($b*$c)) ))
+result["$((counter++))"]=$firstOperation
 echo "$firstOperation"
 
 secondOperation=$(( $(($a*$b))+$c ))
+result["$((counter++))"]=$secondOperation
 echo "$secondOperation"
 
 thirdOperation=$(( $c+$(($a/$b)) ))
+result["$((counter++))"]=$thirdOperation
 echo "$thirdOperation"
 
 fourthOperation=$(( $(($a%$b))+$c ))
+result["$((counter++))"]=$fourthOperation
 echo "$fourthOperation"
+
+echo ${result[@]}
